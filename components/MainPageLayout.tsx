b09c93650eb84e475dc5f5d31d64c623945573da
Ashlyn DeVries <ashjdevries@gmail.com>
2023-01-21 13:19:53 -0500

    Mapped member's name, photo, & team to MenuItem.

--- components/MainPageLayout.tsx
+++ components/MainPageLayout.tsx
@@ -1,8 +1,10 @@
 import { Button, Select, TextField, Typography, createTheme, MenuItem } from "@mui/material"
 import { Box, Container } from "@mui/system"
+import { PropsWithChildren } from "react";
+import Member from "../models/Member";
 
 
-export const MainPageLayout = () => {
+export const MainPageLayout = ({ ce_members }: PropsWithChildren<{ ce_members: Array<Member> }>) => {
     createTheme({
         palette: {
             primary: {
@@ -16,10 +18,20 @@ export const MainPageLayout = () => {
             <Box>
                 <Typography>Create Commendation</Typography>
                 <Select label="To">
-                    <MenuItem>CE Members</MenuItem>
+                    {ce_members.map(ce_member => {
+                        return (
+                            <MenuItem>
+                                <Box sx={{flexDirection: "row"}}>
+                                    <Typography>{ce_member.image_url}</Typography>
+                                    <Typography>{ce_member.name}</Typography>
+                                    <Typography>{ce_member.team}</Typography>
+                                </Box>                            
+                            </MenuItem>
+                        )
+                    })}
                 </Select>
-                <TextField label="Message" variant="filled" />
-                <Button variant="contained" disabled color="primary">SEND</Button>
+                <TextField label="Message" variant="filled" /*onChange={() => }*//>
+                <Button variant="contained" disabled color="primary" /*onClick={() => }*/>SEND</Button>
             </Box>
         </Container>
     )
