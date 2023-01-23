43b211f64266b6c7ef89d2f7f71ffaedd5cf78ce
kcouv <kcouvion@cedarville.edu>
2023-01-23 16:42:02 -0500

    fixed overlap of header and form

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,21 @@
-import { createTheme, AppBar, Toolbar, IconButton, Typography } from "@mui/material"
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material"
 import { Avatar, Box } from "@mui/material"
 
 export const Header = () => {
   return (
-    <AppBar>
-      <Toolbar>
-        <Box>
-          <Box component="img"></Box>
-          <Box component="img"></Box>
-          <Box component="img"></Box>
-        </Box>
-        <Typography>Commendations</Typography>
-        <IconButton></IconButton>
-        <Avatar></Avatar>
-      </Toolbar>
-    </AppBar>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Box>
+            <Box component="img" alt="Bravo Flag" src=""></Box>
+            <Box component="img" alt="Zulu Flag" src=""></Box>
+            <Box component="img" alt="BZ Logo" src=""></Box>
+          </Box>
+          <Typography>Commendations</Typography>
+          <IconButton></IconButton>
+          <Avatar></Avatar>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
