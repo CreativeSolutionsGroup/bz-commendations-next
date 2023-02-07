3bc63952b6189ea2fc2106f9c496c36da89aa8e8
Alex Taylor <52684858+awtpi314@users.noreply.github.com>
2023-02-07 11:35:32 -0500

    added go to home function for top left header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,17 +6,20 @@ import bravo from "../assets/BZ-flag-red.png"
 import zulu from "../assets/BZ-flag.png"
 import bz from "../assets/BZ-letters.png"
 import Image from "next/image"
+import Link from "next/link"
 
 export const Header = () => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <Stack>
-          <Box><Image width={35} height={20} alt="Bravo Flag" src={bravo.src}></Image></Box>
-          <Box><Image width={35} height={20} alt="Zulu Flag" src={zulu.src}></Image></Box>
-        </Stack>
-        <Box ml={1.5}><Image width={50} height={35} alt="BZ Logo" src={bz.src} /></Box>
-        <Typography ml={0.5} fontFamily="fantasy" fontSize={25}>COMMENDATIONS</Typography>
+        <Box display={"flex"} flexDirection={"row"} onClick={(e) => window.open("/", "_self")}>
+          <Stack>
+            <Box><Image width={35} height={20} alt="Bravo Flag" src={bravo.src}></Image></Box>
+            <Box><Image width={35} height={20} alt="Zulu Flag" src={zulu.src}></Image></Box>
+          </Stack>
+          <Box ml={1.5} mt={0.6}><Image width={50} height={35} alt="BZ Logo" src={bz.src} /></Box>
+          <Typography ml={0.5} mt={0.6} fontFamily="fantasy" fontSize={25}>COMMENDATIONS</Typography>
+        </Box>
 
         <Box ml="auto" display="flex">
 
